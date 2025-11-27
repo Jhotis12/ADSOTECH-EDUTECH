fbf11e3a1083db1b92952bdb8556c9d4da666f34
jhonatan <137637410+Jhotis12@users.noreply.github.com>
2025-11-27 12:31:55 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './layouts/MainLayout';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import FAQ from './pages/FAQ';
+import Procedures from './pages/Procedures';
+import Analysis from './pages/Analysis';
+import Resources from './pages/Resources';
+import './index.css'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Dashboard />} />
+          <Route path="faq" element={<FAQ />} />
+          <Route path="tramites" element={<Procedures />} />
+          <Route path="analisis" element={<Analysis />} />
+          <Route path="recursos" element={<Resources />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
